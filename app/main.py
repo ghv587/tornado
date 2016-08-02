8f9d86907b62d1979de2356b8d44c00d3a27a068
ghv587 <zhang_zhenpeng@163.com>
2016-08-02 09:02:52 +0800

    main

--- app/main.py
+++ app/main.py
@@ -1,13 +1,30 @@
 import tornado.ioloop
 import tornado.web
+import tornado.auth
+import tornado.httpserver
+import os
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-	pass
-application = tornado.web.Application([
-    (r"/", ../template/index.html),
-])
+class IndexHandle(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.render('../template/index.html')
+
+
+class WebApplication(tornado.web.Application):
+    def __init__(self):
+        handler = [
+            (r"/", IndexHandle),
+                    ]
+        super(WebApplication, self).__init__(handler)
+
+        settings = {
+            'template_path':os.path.join(os.path.dirname(__file__),'template'),
+            'static_path':os.path.join(os.path.dirname(__file__),'static')
+
+        }
+
+
 
 if __name__ == "__main__":
-    application.listen(8888)
+    http_server = tornado.httpserver.HTTPServer(WebApplication())
+    http_server.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
